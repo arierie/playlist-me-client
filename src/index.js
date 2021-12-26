cdf2970370ffbd85da7fd317ba3245395b0cf9a6
arierie <arieridwansyah@gmail.com>
2021-12-26 20:50:14 +0700

    [Feat] Fix background color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles'
+import './scripts'
+import IndexView from './views/IndexView'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <IndexView/>,
   document.getElementById('root')
 );
 

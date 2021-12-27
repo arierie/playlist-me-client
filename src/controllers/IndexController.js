ac58a1d77dcb022d717c315155e797544229a6e4
arierie <arieridwansyah@gmail.com>
2021-12-27 19:09:18 +0700

    [Fix] Implement connect wallet with metamask

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,15 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import IndexView from '../views/IndexView'
 
-class IndexController extends React.Component {
-    
-    render() {
-        return (
-            <IndexView>
-                <btn-connect onClick={null} />
-            </IndexView>
-        )
-    };
-}
+const IndexController = () => {
 
-export default IndexController;
+ const [currentAccount, setCurrentAccount] = useState("");
+
+ const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+  
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const listenToWalletConnectionStatus = () => {
+    window.ethereum.on("accountsChanged", accounts => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0]);
+        else setCurrentAccount("");
+      });
+    }
+    
+    function onMouseEnter() {
+        return currentAccount != "" ? "Disconnect" : "Connect my wallet"
+    }
+    
+    function onMouseLeave() {
+        return currentAccount != "" ? "Connected" : "Connect my wallet"
+    }
+  
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    listenToWalletConnectionStatus();
+  }, [])
+    
+  return (
+        <IndexView>
+            <btn-connect onClick={connectWallet} onMouseEnter={onMouseEnter()} onMouseLeave={onMouseLeave()} >
+                {onMouseLeave()}
+            </btn-connect>
+        </IndexView>
+    );
+  }
+  
+export default IndexController
